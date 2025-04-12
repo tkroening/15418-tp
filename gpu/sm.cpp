771a4af4c2adf1d808e4afd08edf88b5a5ee9028
Ethan7894 <eblu@andrew.cmu.edu>
2025-04-12 16:13:16 -0400

    started sm

--- gpu/sm.cpp
+++ gpu/sm.cpp
@@ -1,9 +1,91 @@
 #include "sm.h"
 
-#include <stdio.h>
 #include <iostream>
+#include <stdio.h>
 
 // Constructor
-SM::SM() {
-    std::cout << "SM constructor" << std::endl;
+SM::SM(void (*memOpCallback)(int, int64_t), ProcessorArgs args, processor *self,
+       trace_reader *tr, cache *cs, branch *bs, int activeWarps, int smid)
+    : memOpCallback_(memOpCallback), args_(args), ps_(self), tr_(tr), cs_(cs),
+      bs_(bs), smid_(smid) {
+
+  instructionCount_ = 0; // question: do I need self????
+  // initialize registers and state of each warp
+  for (int i = 0; i < MAXWARPS; i++) {
+    warp_t *currWarp = &(warps[i]); // question: do I need self.warps[i]????
+
+    // only intialize active warps
+    if (i < activeWarps) {
+      // all warps are initially runnable
+      currWarp->warpState = RUNNABLE;
+
+      // Initialize the register file to have all registers be ready.
+      for (int reg = 0; reg < REGISTER_COUNT; reg++) {
+        currWarp->rf_[reg] = {.register_num = reg, .tag = 0, .ready = true};
+      }
+    } else {
+      currWarp->warpState = UNINITIALIZED;
+    }
+  }
+
+  /** @brief moves all ops onto instruction queue of warps */
+  trace_op *op;
+  while (true) {
+    op = tr_->getNextOp(pid_);
+    // if we reach end of trace file we break out of loop
+    if (op == NULL) {
+      prinf("we got an null op!!\n");
+      break;
+    } else {
+      // adds the op onto the instruction queue of all initialized threads
+      for (int i = 0; i < MAXWARPS; i++) {
+        warp_t *currWarp = &(warps[i]);
+        if (currWarp->warpState != UNINITIALIZED)
+          (currWarp->dq_).push_back({op, i});
+      }
+      instructionCount_++;
+      assert((warps[0].dq_).size() == instructionCount_);
+    }
+  }
+  assert((warps[0].dq_).size() == instructionCount_);
+
+  // QUESTION: when we read all ops do we
+
+  // initialize insturction queue of each warp
+
+  std::cout << "SM constructor" << std::endl;
+}
+
+/******************************************************************************
+ Fetch
+ ******************************************************************************/
+
+/**
+ * @brief Performs the instruction fetch stage of the pipeline.
+ *
+ * Schedules the warp to execute the next struction
+ * @return True if any instruction can be executed next
+ */
+
+bool Processor::Fetch() {
+  bool progress{false};
+
+  std::pair<trace_op *, uint64_t> instrPair = scheduler();
+
+  trace_op *currentInstruction = instrPair.first;
+  int warpNumber = instrPair.second;
+
+  warp_t *scheduledWarp = &(warps[warpNumber]);
+
+  // update register files
+  for (auto src : I->src_reg) {
+    // If the register is -1, it means no register is required.
+    if (src == -1) {
+      continue;
+    }
+    scheduledWarp->rf_[src].ready = false;
+  }
+
+  deq_.push(instrPair);
+  return progress;
 }
--- gpu/sm.h
+++ gpu/sm.h
@@ -1,11 +1,115 @@
-class SM {
-    public:
-        // Constructor
-        SM ();
+#include <array>
+#include <cstdint>
+#include <queue>
+#include <unordered_map>
 
-        bool Fetch();
-        bool Decode();
-        bool Execute();
-        bool Mem();
-        bool WriteBack();
+extern "C" {
+#include "branch.h"
+#include "cache.h"
+#include "trace.h"
+}
+
+typedef struct {
+  int regNum; /** @brief The register's architectural number. */
+  bool ready; /** @brief Whether the register's value is ready or being
+                 calculated. */
+} Register_t;
+
+/** @brief The arguments that need to be given to the Processor. */
+struct ProcessorArgs {
+  int d; /** Dispatch queue multiplier */
+  int f; /** Fetch rate (instructions per cycle) */
+  int m; /** Schedule queue multiplier */
+  int j; /** Number of "fast" ALUs */
+  int k; /** Number of "long" ALUs */
+  int c; /** Number of CDBs */
+};
+
+/** @brief A warp can either be running, runnable, stalled, or not initialized .
+ */
+typedef enum state {
+  RUNNING, // multiple warps can be running at same time due to piplelined arch
+  RUNNABLE,
+  STALLED, // when memory stalls
+  UNINITIALIZED,
+  FINISHED
+} state_t;
+
+typedef struct warp {
+  state_t warpState;
+  /** @brief The register file, need 1 for each warp */
+  std::array<Register_t, REGISTER_COUNT> rf_;
+
+  /** @brief The instruction queue, storing trace ops and their ids.
+      need 1 for each warp*/
+  std::deque<std::pair<trace_op *, uint64_t>>
+      dq_; // need double queue as you need to peak at instructions in the
+           // front
+
+  /** @brief Sorted queue of finished instructions. */
+  std::vector<std::pair<proc_sq_slot_t, int>> finished_instructions_;
+} warp_t;
+
+class SM {
+public:
+  // Constructor
+  SM(void (*memOpCallback)(int, int64_t), ProcessorArgs args, processor *self,
+     trace_reader *tr, cache *cs, branch *bs, int activeWarps, int smid);
+
+  /** @brief The processor simulator pointer. */
+  processor *ps_;
+
+  /** @brief The trace reader pointer. */
+  trace_reader *tr_;
+
+  /** @brief The cache simulator pointer. */
+  cache *cs_;
+
+  /** @brief The branch predictor simulator pointer, not current focus*/
+  branch *bs_;
+
+  int instructionCount_;
+
+  bool Fetch();
+  bool Decode();
+  bool Execute();
+  bool Mem();
+  bool WriteBack();
+
+private:
+  /** @brief The sm number, currently just doing 1 */
+  int smid_;
+
+  /** @brief the number of warps in use */
+  int activeWarps_;
+
+  /** @brief structure that stores all info about warps, similar to thread
+   * control block*/
+  std::array<warp_t, MAXWARPS> warps;
+
+  /** @brief Whether  a pending branch request. */
+  std::optional<uint64_t> pending_branch_;
+
+  /** @brief Function pointer for memOpCallback */
+  void (*memOpCallback_)(int, int64_t);
+
+  /** @brief The queue of instructions going into decode stage
+      produced by fetch and consumed by decode*/
+  std::queue<std::pair<trace_op *, uint64_t>>
+      deq_; // should always have length 0 or 1;
+
+  /** @brief The queue of instructions going into decode stage
+       produced by deocde and consumed by execute*/
+  std::queue<std::pair<trace_op *, uint64_t>>
+      deq_; // should always have length 0 or 1;
+
+  /** @brief The queue of instructions going into memory stage
+      produced by execute and consumed by memory*/
+  std::queue<std::pair<trace_op *, uint64_t>>
+      memq_; // should always have length 0 or 1;
+
+  /** @brief The queue of instructions going into write back stage
+     produced by memory and consumed by wb */
+  std::queue<std::pair<trace_op *, uint64_t>>
+      wbq_; // should always have length 0 or 1;
 };
